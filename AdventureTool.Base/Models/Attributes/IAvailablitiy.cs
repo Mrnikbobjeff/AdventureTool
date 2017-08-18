0b51163cccb1f63aa83acb555caea776fab4d6b5
schilli <schilli@adesso-mobile.de>
2017-08-18 21:29:10 +0200

    Other: added furhter interfaces

---
+++ AdventureTool.Base/Models/Attributes/IAvailablitiy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureTool.Base.Models.Attributes
+{
+    public interface IAvailablitiy
+    {
+        int Availability { get; }
+    }
+}
---
+++ AdventureTool.Base/Models/Interfaces/IItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureTool.Base.Models.Interfaces
+{
+    public interface IItem
+    {
+        string Name { get; set; }
+
+        
+    }
+}
