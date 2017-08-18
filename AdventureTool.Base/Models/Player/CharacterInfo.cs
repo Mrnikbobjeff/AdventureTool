ea6d5794bd21d1d45dc1d39dd3f56e74bbbeac5e
schilli <schilli@adesso-mobile.de>
2017-08-18 21:17:44 +0200

    Other: Added behaviour interfaces

---
+++ AdventureTool.Base/Models/Player/CharacterInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventureTool.Repositories.Database;
+using Realms;
+
+namespace AdventureTool.Base.Models.Player
+{
+    class CharacterInfo : RealmObject, IEntity
+    {
+        public string Id { get; set; }
+        public string Provider { get; set; }
+        public bool Deleted { get; set; }
+        public DateTimeOffset LastModified { get; set; }
+    }
+}
