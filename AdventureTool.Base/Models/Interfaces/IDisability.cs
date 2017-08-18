fa5d4cd85f957a6417e8ac83bc82e7345c1ad3d1
Niklas Schilli <Schilli@adesso-mobile.de>
2017-08-18 22:11:41 +0200

    Other: Talents modified

---
+++ AdventureTool.Base/Models/Interfaces/IDisability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureTool.Base.Models.Interfaces
+{
+    interface IDisability
+    {
+        int AddModifier { get; set; }
+        int MultModifier { get; set; }
+
+    }
+}
